8d03ec98bceb8789916e5049bec4c11c0db9f81f
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2024-07-08 12:28:31 -0600

    Se añade dashboard y pagina de contador con server y side rendering

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,15 @@
+import CartCounter from '@/app/shopping-cart/components/CartCounter';
+
+export const metadata = {
+    title: 'Counter page',
+    description: 'Contador',
+};
+
 const page = () => {
     return (
-        <div>
-            <h1>Page Counter</h1>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <span>Productos en el carrito</span>
+            <CartCounter value={10} />
         </div>
     );
 };
