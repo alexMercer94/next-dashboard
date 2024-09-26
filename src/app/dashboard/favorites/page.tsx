5d45c760e198a71676248e6c41ba24529259556a
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2024-09-26 12:24:50 -0600

    feature: slices para pokemons y favoritos, middleware

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -1,13 +1,19 @@
-import { PokemonGrid } from '@/pokemons';
+import { FavoritePokemons } from '@/pokemons';
 
-const FavoritesPage = async () => {
+export const metadata = {
+    title: 'Favoritos',
+    description: 'Loremhfurofh cioej jfieo',
+};
+
+const FavoritesPage = () => {
     return (
         <div className="flex flex-col">
             <span className="text-5xl my-2">
                 Pokémons Favoritos <small className="text-blue-500">Global State</small>{' '}
             </span>
 
-            <PokemonGrid pokemons={[]} />
+            {/* <PokemonGrid pokemons={Object.values(pokemons)} /> */}
+            <FavoritePokemons />
         </div>
     );
 };
--- src/store/Providers.tsx
+++ src/store/Providers.tsx
@@ -1,13 +1,19 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '.';
+import { setFavortitePokemons } from './pokemons/pokemons';
 
 interface Props {
     children: React.ReactNode;
 }
 
 export const Providers = ({ children }: Props) => {
+    useEffect(() => {
+        const favorites = JSON.parse(localStorage.getItem('favorites-pokemons') ?? '{}');
+        store.dispatch(setFavortitePokemons(favorites));
+    }, []);
+
     return <Provider store={store}>{children}</Provider>;
 };
